f424f187576924d711f2b0db52165eff7d91c54c
t3rb3d <terbed@gmail.com>
2024-10-21 22:53:17 +0200

    Add other bar type classes

--- finmlkit/bars/kit.py
+++ finmlkit/bars/kit.py
@@ -38,3 +38,105 @@ class TimeBarKit(BarBuilderBase):
         """
         timestamps = self._raw_data['timestamp'].astype(np.int64).values
         return _time_bar_indexer(timestamps, self.interval)
+
+
+class TickBarKit(BarBuilderBase):
+    """
+    Tick bar builder class.
+    """
+
+    def __init__(self, trades: pd.DataFrame, tick_count_thrs: int):
+        """
+        Initialize the tick bar builder with raw trades data and tick count.
+
+        Parameters
+        ----------
+        trades : pd.DataFrame
+            A dataframe containing raw trades data with columns 'timestamp', 'price', and 'amount'.
+        tick_count : int
+            The tick count for the tick bars.
+        """
+        super().__init__(trades)
+        self.tick_count_thrs = tick_count_thrs
+
+        logger.info(f"Tick bar builder initialized with tick count: {tick_count_thrs}.")
+
+    def _generate_bar_opens(self) -> Tuple[NDArray[np.int64], NDArray[np.int64]]:
+        """
+        Generate tick bar indices using the tick bar indexer.
+        Returns
+        -------
+        tuple(np.array(np.int64), np.array(np.int64))
+            Open timestamps and corresponding open indices in the raw trades data.
+        """
+        timestamps = self._raw_data['timestamp'].astype(np.int64).values
+        return _tick_bar_indexer(timestamps, self.tick_count_thrs)
+
+
+class VolumeBarKit(BarBuilderBase):
+    """
+    Volume bar builder class.
+    """
+
+    def __init__(self, trades: pd.DataFrame, volume_thrs: float):
+        """
+        Initialize the volume bar builder with raw trades data and volume.
+
+        Parameters
+        ----------
+        trades : pd.DataFrame
+            A dataframe containing raw trades data with columns 'timestamp', 'price', and 'amount'.
+        volume : float
+            The volume for the volume bars.
+        """
+        super().__init__(trades)
+        self.volume_thrs = volume_thrs
+
+        logger.info(f"Volume bar builder initialized with volume: {volume_thrs}.")
+
+    def _generate_bar_opens(self) -> Tuple[NDArray[np.int64], NDArray[np.int64]]:
+        """
+        Generate volume bar indices using the volume bar indexer.
+        Returns
+        -------
+        tuple(np.array(np.int64), np.array(np.int64))
+            Open timestamps and corresponding open indices in the raw trades data.
+        """
+        timestamps = self._raw_data['timestamp'].astype(np.int64).values
+        volumes = self._raw_data['amount'].values
+        return _volume_bar_indexer(timestamps, volumes, self.volume_thrs)
+
+
+class DollarBarKit(BarBuilderBase):
+    """
+    Dollar bar builder class.
+    """
+
+    def __init__(self, trades: pd.DataFrame, dollar_thrs: float):
+        """
+        Initialize the dollar bar builder with raw trades data and dollar amount.
+
+        Parameters
+        ----------
+        trades : pd.DataFrame
+            A dataframe containing raw trades data with columns 'timestamp', 'price', and 'amount'.
+        dollar : float
+            The dollar amount for the dollar bars.
+        """
+        super().__init__(trades)
+        self.dollar_thrs = dollar_thrs
+
+        logger.info(f"Dollar bar builder initialized with dollar amount: {dollar_thrs}.")
+
+    def _generate_bar_opens(self) -> Tuple[NDArray[np.int64], NDArray[np.int64]]:
+        """
+        Generate dollar bar indices using the dollar bar indexer.
+        Returns
+        -------
+        tuple(np.array(np.int64), np.array(np.int64))
+            Open timestamps and corresponding open indices in the raw trades data.
+        """
+        timestamps = self._raw_data['timestamp'].astype(np.int64).values
+        prices = self._raw_data['price'].values
+        volumes = self._raw_data['amount'].values
+        return _dollar_bar_indexer(timestamps, prices, volumes, self.dollar_thrs)
