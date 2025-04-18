6bb3ac20d47e9a4ce7f09cc5735f48141e3c6d46
t3rb3d <terbed@gmail.com>
2025-04-18 12:09:53 +0200

    minor setups

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # setup.py
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='finmlkit',
-    version='0.1',
-    packages=find_packages(),  # will find the inner finmlkit package
-    install_requires=[
-        # add dependencies if needed
-    ]
+    name="finmlkit",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requirements,
 )
