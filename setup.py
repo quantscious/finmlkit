03d6134abb7cebadcd74bd8769f8fbb35c49c768
t3rb3d <terbed@gmail.com>
2025-08-02 11:05:35 +0200

    add deploy workflow upon new release

--- setup.py
+++ setup.py
@@ -7,6 +7,19 @@ with open("requirements.txt") as f:
 setup(
     name="finmlkit",
     version="0.1",
-    packages=find_packages(),
+    description="Financial ML toolkit",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Dániel Terbe",
+    url="https://github.com/quantscious/finmlkit",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.9",
     install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
